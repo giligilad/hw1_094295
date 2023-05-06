5d95f29fce6199109424c28cce26000120b81750
ggilad <gili_gilad@intuit.com>
2023-05-06 15:52:26 +0300

    gili branch: add save csv to unseen data

--- 05may23/predict.py
+++ 05may23/predict.py
@@ -3,34 +3,34 @@ import pickle
 import pandas as pd
 import os
 
-# load data:
-# train_directory = 'data/test/'
-# all_train_files = [file for file in os.listdir(train_directory) if file.endswith('.psv')]
-# combined_df = pd.DataFrame()
-# for i, file in enumerate(all_train_files):
-#     if i % 1000 == 0:
-#         print(i)
-#     file_path = train_directory + file
-#     df = pd.read_csv(file_path, delimiter='|')
-#     id = file_path.split("patient_")[-1].strip(".psv")
-#     df['id'] = [id] * len(df)
-#     df['label_change'] = df.SepsisLabel.diff().fillna(0) != 0
-#     if (df['label_change'].sum() == 2):
-#         shift_index = df.loc[df['label_change'].idxmax(), :].name
-#         df = df.loc[:shift_index, :]
-#     elif (df['SepsisLabel'][0] == 1):
-#         df = df.iloc[[0], :]
-#     if i == 0:
-#         combined_df = df
-#     else:
-#         combined_df = pd.concat([combined_df, df], ignore_index=True)
-# combined_df.reset_index(inplace=True)
-# print(combined_df.columns)
-#combined_df.to_csv("new_test_data_all_cols.csv", index=False)
+#load data:
+train_directory = 'data/test/'
+all_train_files = [file for file in os.listdir(train_directory) if file.endswith('.psv')]
+combined_df = pd.DataFrame()
+for i, file in enumerate(all_train_files):
+    if i % 1000 == 0:
+        print(i)
+    file_path = train_directory + file
+    df = pd.read_csv(file_path, delimiter='|')
+    id = file_path.split("patient_")[-1].strip(".psv")
+    df['id'] = [id] * len(df)
+    df['label_change'] = df.SepsisLabel.diff().fillna(0) != 0
+    if (df['label_change'].sum() == 2):
+        shift_index = df.loc[df['label_change'].idxmax(), :].name
+        df = df.loc[:shift_index, :]
+    elif (df['SepsisLabel'][0] == 1):
+        df = df.iloc[[0], :]
+    if i == 0:
+        combined_df = df
+    else:
+        combined_df = pd.concat([combined_df, df], ignore_index=True)
+combined_df.reset_index(inplace=True)
+print(combined_df.columns)
+combined_df.to_csv("unseen_data.csv", index=False)
 
 
 # read the aggregate csv:
-combined_df = pd.read_csv('agg_last_data_test_new.csv', index_col=0)
+combined_df = pd.read_csv('unseen_data.csv', index_col=0)
 all_columns = ['ICULOS', 'Temp', 'HR', 'WBC', 'BUN', 'Resp', 'Calcium', 'DBP', 'Hct', 'Hgb', 'MAP', 'HospAdmTime',
              'Creatinine', 'SBP', 'Glucose', 'Magnesium', 'Gender', 'O2Sat', 'Potassium', 'Age','id','SepsisLabel']
 x_columns = all_columns[:-1]
